25d4527f090ee8436299255b96df9959a0fad4ec
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-11-07 12:26:33 +0000

    remove

--- tmp.ts
+++
@@ -1,9 +0,0 @@
-function teste () {
-    if (1+1 == 2) {
-        console.log("Hello World");
-    } else if (1 + 1 !== 2) {
-        console.log("Hello World");
-    } else {
-        console.log("else clause")
-    }
-}
