6003bc725509261c0b3385720159f4a49363503b
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-01-07 12:54:04 +0000

    update

---
+++ publish.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+set -e
+
+# release defaults to patch (last number in semver)
+RELEASE="patch" && [ -n "$1" ] && RELEASE=$1
+
+# cut the release
+VERSION=$(npm --no-git-tag-version version $RELEASE | sed 's/v//')
+git add package.json
+git commit -m "release: cut the $VERSION release"
+
+# tag the release
+git tag $VERSION
+git tag -l
+
+echo -e "\033[1;92m You are ready to publish!"
+echo -e "\033[1;95m git push"
+echo -e "\033[1;95m git push --tags"
+echo -e "\033[1;95m npm publish"
